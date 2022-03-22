f1cd7bd75173450726e6e166a230d6b92198e5be
Joshua Ayeni <larryjmpk@gmail.com>
2022-03-22 15:58:40 +0100

    fix static file serving issue

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = config.get('port')
 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
-app.use('/public',express.static(__dirname + '/public'))
+app.use('/',express.static(__dirname + '/public'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors(corsOption))

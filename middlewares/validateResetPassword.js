da96752df2f085111c512e8f71dd0de29119728a
Joshua Ayeni <larryjmpk@gmail.com>
2022-06-14 15:13:28 +0100

    modified token message

--- middlewares/validateResetPassword.js
+++ middlewares/validateResetPassword.js
@@ -1,9 +1,9 @@
 const User = require('../models/registeredUser')
 
 module.exports = async function(req,res,next){
-    if(req.params.token == undefined) return res.status(400).send({message:'Token not found'})
+    if(req.params.token == undefined) return res.status(400).send({message:'Token not found, please try the process again'})
     const user = await User.findOne({token:req.params.token})
-    if(!user) return res.status(400).send({message:'Token does not exist'})
+    if(!user) return res.status(400).send({message:'Token does not exist, please try the process again'})
     req.token = user
 
     next()

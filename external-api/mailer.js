0bb36090ef1077a012173c51fe8940d9e324dd2f
Joshua Ayeni <larryjmpk@gmail.com>
2022-07-13 02:13:39 +0100

    removed mailersend

--- external-api/mailer.js
+++ external-api/mailer.js
@@ -1,36 +1,36 @@
-const Recipient = require("mailersend").Recipient;
-const EmailParams = require("mailersend").EmailParams;
-const MailerSend = require("mailersend");
-const config = require('config')
+// const Recipient = require("mailersend").Recipient;
+// const EmailParams = require("mailersend").EmailParams;
+// const MailerSend = require("mailersend");
+// const config = require('config')
 
 
-module.exports = mailer  = {
+// module.exports = mailer  = {
 
-    mailersend : function(params){
-        new MailerSend({
-            api_key: config.get('MAILER_SEND_API_TOKEN'),
-        }).send(params)
-    },
+//     mailersend : function(params){
+//         new MailerSend({
+//             api_key: config.get('MAILER_SEND_API_TOKEN'),
+//         }).send(params)
+//     },
 
-    emailHtmlParams : function(recipients,subject,html){
-        return new EmailParams()
-        .setFrom("support@lilonghero.com")
-        .setFromName("Lilonghero")
-        .setRecipients(recipients)
-        .setSubject(subject)
-        .setHtml(html)
-    },
+//     emailHtmlParams : function(recipients,subject,html){
+//         return new EmailParams()
+//         .setFrom("support@lilonghero.com")
+//         .setFromName("Lilonghero")
+//         .setRecipients(recipients)
+//         .setSubject(subject)
+//         .setHtml(html)
+//     },
 
-    emailTextParams : function(recipients,subject,text){
-        let newRecipients = recipients.map(function(ele){
-            return new Recipient(ele, "Recipient")
-        })
-        return new EmailParams()
-        .setFrom("support@lilonghero.com")
-        .setFromName("Lilonghero")
-        .setRecipients(newRecipients)
-        .setSubject(subject)
-        .setText(text);
-    }
+//     emailTextParams : function(recipients,subject,text){
+//         let newRecipients = recipients.map(function(ele){
+//             return new Recipient(ele, "Recipient")
+//         })
+//         return new EmailParams()
+//         .setFrom("support@lilonghero.com")
+//         .setFromName("Lilonghero")
+//         .setRecipients(newRecipients)
+//         .setSubject(subject)
+//         .setText(text);
+//     }
 
-}
+// }
